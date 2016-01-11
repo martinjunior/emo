f1705c1b437de5a0261aa42895d8cbf689695c5c
Martin Duran <martinduran@mduran-osx.local>
2016-01-11 16:01:40 -0600

    Changed emo's description

--- tasks/emo.js
+++ tasks/emo.js
@@ -12,7 +12,7 @@ var StyleGuide = require('./components/StyleGuide');
 
 module.exports = function(grunt) {
 
-    grunt.registerMultiTask('emo', 'The best Grunt plugin ever.', function() {
+    grunt.registerMultiTask('emo', 'A style-guide generator.', function() {
         var styleGuide = new StyleGuide(grunt, this.files, this.options());
     });
 
