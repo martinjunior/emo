5a7c85fd90fd1a39a7ad804f016c7da7b64dcabe
Martin Duran <martinduran@mduran-osx.local>
2016-02-17 13:41:49 -0600

    Updating/fixing componentizer

--- tasks/components/Componentizer.js
+++ tasks/components/Componentizer.js
@@ -3,59 +3,90 @@
 
     var fs = require('fs');
     var path = require('path');
+    var marked = require('marked');
+    var merge = require('../utils/merge');
     var regex = require('../utils/regexs');
+    var fileExists = require('../utils/fileExists');
     var Scraper = require('./Scraper');
-    var Componentizer = require('./Componentizer');
 
-    var Viewizer = function(files, delimiters) {
-        Componentizer.call(this, files, delimiters);
-    };
+    var Componentizer = function(files, delimiters) {
+        this.files = files;
 
-    Viewizer.prototype = Object.create(Componentizer.prototype);
-    Viewizer.prototype.constructor = Componentizer;
-
-    var proto = Viewizer.prototype;
-
-    proto.init = function() {
-        var srcFiles = [];
-
-        this.files.forEach(function(fileMapping) {
-            fileMapping.forEach(function(filePair) {
-                filePair.src.forEach(function(src) {
-                    srcFiles.push(src);
-                }.bind(this));
-            });
-        }.bind(this));
-
-        return this.scrape(srcFiles);
-    };
-
-    proto.process = function(view) {
-        this.files.some(function(fileMapping) {
-            var match = false;
-
-            fileMapping.some(function(filePair) {
-                filePair.src.some(function(src) {
-                    var isFile = fs.lstatSync(src).isFile();
-
-                    match = (src === view._basepath + view._basename) && isFile;
-
-                    if (match) {
-                        view.path = filePair.dest
-                    }
-
-                    return match;
-                });
-
-                return match;
-            });
-
-            return match;
+        this.scraper = new Scraper({
+            delimiters: delimiters
         });
 
-        return view;
+        this.data = {};
+
+        this.init();
     };
 
-    module.exports = Viewizer;
+    Componentizer.OPTIONS = {
+        propsToProcess: ['description']
+    };
+
+    Componentizer.readFile = function(filePath) {
+        return fs.readFileSync(
+            filePath,
+            Scraper.OPTIONS.readFileSync
+        ).replace(/^\uFEFF/, '')
+    };
+
+    var proto = Componentizer.prototype;
+
+    proto.init = function() {
+        return this.scrape(this.files);
+    };
+
+    proto.scrape = function(files) {
+        var data = {};
+
+        this.scraper.scrape(files);
+
+        this.scraper.getData()
+            .map(this.process.bind(this))
+            .sort(this.sort.bind(this))
+            .forEach(function(component) {
+                var category = component.category;
+
+                if (!data[category]) {
+                    data[category] = [];
+                }
+
+                data[category].push(component);
+            }.bind(this));
+
+        this.data = data;
+    };
+
+    proto.get = function() {
+        return this.data;
+    };
+
+    proto.sort = function(a, b) {
+        return a.category > b.category;
+    };
+
+    proto.process = function(component) {
+        Object.keys(component).forEach(function(key) {
+            var filePath = component._basepath + component[key];
+
+            if (Componentizer.OPTIONS.propsToProcess.indexOf(key) === -1) {
+                return;
+            }
+
+            component[key] = marked(
+                fileExists(filePath)
+                ? Componentizer.readFile(filePath)
+                : component[key]
+            );
+        });
+
+        return merge(component, {
+            path: path.join(component.category, component.filename || component.name + '.html')
+        });
+    };
+
+    module.exports = Componentizer;
 
 } ());
